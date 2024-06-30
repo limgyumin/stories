dab77697300dbec2edddc91c4e0162d179ebff0f
limgyumin <dev.limgyumin@gmail.com>
2024-06-30 23:00:03 +0900

    feat: Contact 컴포넌트의 platform 목록 아이템에 title 속성 추가

--- components/ui/footer/contact/contact.constants.tsx
+++ components/ui/footer/contact/contact.constants.tsx
@@ -5,6 +5,7 @@ import { IoLogoGithub } from "react-icons/io5";
 
 export type Platform = Readonly<{
   name: string;
+  title: string;
   url: string;
   icon: ReactNode;
 }>;
@@ -12,11 +13,13 @@ export type Platform = Readonly<{
 export const PLATFORMS: readonly Platform[] = [
   {
     name: "github",
+    title: "Github",
     url: "https://github.com/limgyumin",
     icon: <IoLogoGithub />,
   },
   {
     name: "linkedin",
+    title: "LinkedIn",
     url: "https://www.linkedin.com/in/milimgyu/",
     icon: <FaLinkedin />,
   },
