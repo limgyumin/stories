c1bc6f53b5aa12bf29ea408bec7e4c8fc22a8c10
limgyumin <dev.limgyumin@gmail.com>
2024-06-30 09:05:39 +0900

    style: 기존의 단일 테마 색상 사용처들에 테마별 색상 적용

--- components/ui/footer/contact/contact.tsx
+++ components/ui/footer/contact/contact.tsx
@@ -4,8 +4,11 @@ export const Contact = () => {
   return (
     <div className="flex items-center">
       {PLATFORMS.map((platform) => (
-        <div key={platform.name} className="border-l border-gray-300 px-2 first:border-none first:pl-0 last:pr-0">
-          <a className="text-lg text-gray-500" href={platform.url} target="_blank">
+        <div
+          key={platform.name}
+          className="border-muted-foreground border-l px-2 first:border-none first:pl-0 last:pr-0"
+        >
+          <a className="text-muted-foreground text-lg" href={platform.url} target="_blank">
             {platform.icon}
           </a>
         </div>
--- components/ui/footer/footer.tsx
+++ components/ui/footer/footer.tsx
@@ -3,7 +3,7 @@ import { Contact } from "./contact";
 export const Footer = () => {
   return (
     <footer className="flex h-20 w-full items-center justify-center gap-6">
-      <p className="text-xs font-normal text-gray-500">© 2024 • milimgyu • All rights reserved</p>
+      <p className="text-muted-foreground text-xs font-normal">© 2024 • milimgyu • All rights reserved</p>
       <Contact />
     </footer>
   );
