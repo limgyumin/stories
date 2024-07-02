81b8e8ec00c98421ce5d2656cf872c7b9d664252
limgyumin <dev.limgyumin@gmail.com>
2024-07-03 07:38:38 +0900

    feat: 이미지의 width, height 조회 로직 제거, Thumbnail 이미지 크기 고정

--- components/ui/card/content.tsx
+++ components/ui/card/content.tsx
@@ -8,6 +8,8 @@ type Props = ComponentPropsWithoutRef<"div">;
 export const Content = forwardRef<HTMLDivElement, Props>(({ children, className, ...rest }, ref) => {
   return (
     <>
+      <div className="absolute bottom-0 left-0 right-0 z-[1] h-32 bg-gradient-to-b from-[rgba(0,0,0,0)_5%] to-[rgba(0,0,0,0.5)_80%]" />
+
       <div
         ref={ref}
         className={cx(className, "absolute bottom-0 left-0 right-0 z-[1] flex flex-col gap-1 p-[0px_16px_16px_16px]")}
@@ -15,8 +17,6 @@ export const Content = forwardRef<HTMLDivElement, Props>(({ children, className,
       >
         {children}
       </div>
-
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-b from-[rgba(0,0,0,0)_5%] to-[rgba(0,0,0,0.5)_80%]" />
     </>
   );
 });
