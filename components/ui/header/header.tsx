1562bdbca01a28bb024e383866d73e4aa717c4f0
limgyumin <dev.limgyumin@gmail.com>
2024-07-07 11:25:36 +0900

    style: header 높이 값을 px 에서 rem 으로 변경

--- components/ui/header/header.tsx
+++ components/ui/header/header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 z-10 border-b bg-background/90 backdrop-blur-sm">
       <Container>
-        <div className="flex h-[40px] w-full items-center justify-between">
+        <div className="flex h-10 w-full items-center justify-between">
           <Link href="/" className="text-foreground" title="Go to the home page" aria-label="Go to the home page">
             <Logo />
           </Link>
