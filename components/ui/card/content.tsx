dec279660ff207246ae7f62f39d144dd8d91fe47
limgyumin <dev.limgyumin@gmail.com>
2024-07-07 11:24:48 +0900

    style: 내부 콘텐츠가 더 강조되도록 그라디언트 스타일 개선

--- components/ui/card/content.tsx
+++ components/ui/card/content.tsx
@@ -8,11 +8,11 @@ type Props = ComponentPropsWithoutRef<"div">;
 export const Content = forwardRef<HTMLDivElement, Props>(({ children, className, ...rest }, ref) => {
   return (
     <>
-      <div className="absolute bottom-0 left-0 right-0 z-[1] h-32 bg-gradient-to-b from-[rgba(0,0,0,0)_5%] to-[rgba(0,0,0,0.5)_80%]" />
+      <div className="absolute bottom-0 left-0 right-0 z-[1] h-full bg-gradient-to-b from-[rgba(0,0,0,0)_40%] via-[rgba(0,0,0,0.05)_48%] to-[rgba(0,0,0,0.55)_91%]" />
 
       <div
         ref={ref}
-        className={cx(className, "absolute bottom-0 left-0 right-0 z-[1] flex flex-col gap-1 p-[0px_16px_16px_16px]")}
+        className={cx(className, "absolute bottom-0 left-0 right-0 z-[1] flex flex-col gap-1 px-4 pb-4")}
         {...rest}
       >
         {children}
