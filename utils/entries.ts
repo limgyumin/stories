11de411853cac74d5a7cd13bc1f16d54cecbb39b
limgyumin <dev.limgyumin@gmail.com>
2024-07-19 21:55:51 +0900

    feat: 미사용 함수 제거

--- utils/entries.ts
+++
@@ -1,5 +0,0 @@
-import type { Obj } from "types/utils/obj";
-
-export const entries = <T extends Obj>(obj: T): [keyof T, T[keyof T]][] => {
-  return Object.entries(obj) as [keyof T, T[keyof T]][];
-};
