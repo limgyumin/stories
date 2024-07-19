08c63b78aa0683772b4257fcc2f304a2af99951f
limgyumin <dev.limgyumin@gmail.com>
2024-07-19 21:45:33 +0900

    feat: paragraph block 컴포넌트 구현

---
+++ components/article/block/paragraph-block/paragraph-block.tsx
@@ -0,0 +1,19 @@
+import type { BlockChild } from "libs/notion/notion.types";
+
+import { RichText } from "../rich-text";
+
+type Props = {
+  block: BlockChild<"paragraph">;
+};
+
+export const ParagraphBlock = ({ block }: Props) => {
+  if (block.paragraph.richText.length === 0) {
+    return null;
+  }
+
+  return (
+    <p className="mt-3 py-1 first:!mt-0 md:mt-4">
+      <RichText richText={block.paragraph.richText} />
+    </p>
+  );
+};
