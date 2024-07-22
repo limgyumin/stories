89224338d9f5cea1620974800b185012c50902d8
규민 <dev.limgyumin@gmail.com>
2024-07-23 04:43:27 +0900

    refactor: rotator 컴포넌트 제거 (#20)

--- app/(feed)/page.tsx
+++ app/(feed)/page.tsx
@@ -1,22 +1,15 @@
 import { Articles } from "components/feed/articles";
 import { Container } from "components/layout/container";
 import { fetchArticles } from "repositories/article/article.repository";
-import type { Viewport } from "types/viewport";
-
-type Props = {
-  searchParams: Record<string, string | string[] | undefined>;
-};
 
 export const revalidate = 3600;
 
-const FeedPage = async ({ searchParams }: Props) => {
-  const viewport = searchParams.viewport as Viewport;
-
+const FeedPage = async () => {
   const { results } = await fetchArticles();
 
   return (
     <Container className="min-h-[calc(100vh-9rem)] py-10">
-      <Articles articles={results} viewport={viewport} />
+      <Articles articles={results} />
     </Container>
   );
 };
