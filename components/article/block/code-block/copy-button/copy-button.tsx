f857d026f28362e1789ecaf13f57693d5f3c31ab
규민 <dev.limgyumin@gmail.com>
2024-07-31 18:46:57 +0900

    feat: 코드 복사 버튼 클릭 시 복사됨을 인지시키는 UI 구현 (#28)

--- components/article/block/code-block/copy-button/copy-button.tsx
+++ components/article/block/code-block/copy-button/copy-button.tsx
@@ -1,21 +1,30 @@
 "use client";
 
-import { IoMdCopy } from "react-icons/io";
+import { IoMdCopy, IoMdCheckmark } from "react-icons/io";
+
+import { useRollbackState } from "hooks/use-rollback-state";
 
 type Props = {
   text: string;
 };
 
 export const CopyButton = ({ text }: Props) => {
-  const handleCopyClick = () => copy(text);
+  const [isCopied, setIsCopied] = useRollbackState<boolean>(false, 1.5 * 1000);
+
+  const handleCopyClick = () => {
+    copy(text);
+    setIsCopied(true);
+  };
 
   return (
     <button
-      className="flex cursor-pointer items-center gap-[2px] rounded-md px-[2px] py-[2px] text-neutral-400 opacity-100 transition-all group-hover:opacity-100 md:px-[6px] md:opacity-0 md:hover:bg-neutral-800"
+      className="flex cursor-pointer items-center gap-[2px] rounded-md px-[2px] py-[2px] text-neutral-400 opacity-100 transition-all disabled:text-green-500 group-hover:opacity-100 md:px-[6px] md:opacity-0 md:hover:bg-neutral-800"
+      disabled={isCopied}
       onClick={handleCopyClick}
     >
-      <IoMdCopy size={16} />
-      <div className="hidden text-[11.5px] md:block">Copy</div>
+      {isCopied ? <IoMdCheckmark size={16} /> : <IoMdCopy size={16} />}
+
+      <div className="hidden text-[11.5px] md:block">{isCopied ? "Copied" : "Copy"}</div>
     </button>
   );
 };
